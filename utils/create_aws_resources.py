988ba9a2ebef27356e3c518ad5cef9f4dc547de5
Bharath Kumar V <bstech7878@gmail.com>
2022-01-08 22:52:04 +0530

    create and delete iamrole andpolicy for ecs task

--- utils/create_aws_resources.py
+++ utils/create_aws_resources.py
@@ -1,5 +1,14 @@
 from aws_resources import ecr
 from aws_resources import ecs
+from aws_resources import iam
 
 ecr.create_repo()
+print('Created ECR repository')
 ecs.create_cluster()
+print('Created ECS Cluster')
+iam.create_role()
+print('Created ECS Task Role')
+iam.create_policy()
+print('Created ECS Task Execution Policy')
+iam.attach_policy_to_role()
+print('Attached policy to the role')
--- utils/values.py
+++ utils/values.py
@@ -1,2 +1,36 @@
+ACCOUNT_ID='892933193600'
 ECR_REPO_NAME='flask_app'
 ECS_CLUSTER_NAME='bstech'
+ECS_TASK_ROLE_NAME='ECSTaskRole'
+ECS_TASK_ROLE_DESC='ECS Task Role'
+ECS_TASK_POLICY_NAME='ECSTaskExceutionPolicy'
+ECS_TASK_POLICY_DESC='ECS Task Execution Policy'
+ECS_TASK_ROLE_JSON={
+    "Version": "2012-10-17",
+    "Statement": [{
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ecs-tasks.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+ECS_TASK_POLICY_JSON={
+    "Version": "2012-10-17",
+    "Statement": [{
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+ECS_TASK_POLICY_ARN=f'arn:aws:iam::{ACCOUNT_ID}:policy/{ECS_TASK_POLICY_NAME}'
