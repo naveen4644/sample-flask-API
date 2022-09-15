a6762d645b9950a18debe60ee3666c10f8aea414
Naveen Thakur <naveenthakur@visions.net.in>
2022-09-15 16:34:30 +0530

    update

--- utils/aws_resources/loadbalncer.py
+++ utils/aws_resources/loadbalncer.py
@@ -1,60 +1,71 @@
 import boto3
 client = boto3.client('elbv2')
 
-response = client.create_target_group(
-    Name='test-orthos-target-group',
-    Protocol='HTTP',
-    ProtocolVersion='HTTP1',
-    Port=5005,
-    VpcId='vpc-062af964db13d2f63',
-    HealthCheckProtocol='HTTP',
+def create_target_group():
+    try:
+        response = client.create_target_group(
+            name='test-orthos-target-group',
+            Protocol='HTTP',
+            ProtocolVersion='HTTP1',
+            Port=5005,
+            VpcId='vpc-062af964db13d2f63',
+            HealthCheckProtocol='HTTP',
    
-    HealthCheckEnabled=True,
-    HealthCheckPath='/',
-    HealthCheckIntervalSeconds=30,
-    HealthCheckTimeoutSeconds=5,
-    HealthyThresholdCount=5,
-    UnhealthyThresholdCount=2,
-    Matcher={
-        'HttpCode': '302',
+            HealthCheckEnabled=True,
+            HealthCheckPath='/',
+            HealthCheckIntervalSeconds=30,
+            HealthCheckTimeoutSeconds=5,
+            HealthyThresholdCount=5,
+            UnhealthyThresholdCount=2,
+            Matcher={
+                'HttpCode': '302',
        
-    },
-    TargetType='ip',
-    Tags=[
-        {
-            'Key': 'string',
-            'Value': 'string'
-        },
-    ],
-    IpAddressType='ipv4'
-)
+            },  
+            TargetType='ip',
+            Tags=[
+                {
+                    'Key': 'string',
+                    'Value': 'string'
+                },
+            ],
+            IpAddressType='ipv4'
+        )
+        return response['targetgroup']
+    except Exception as e:
+        print(str(e))
+        return False
 
-response = client.create_load_balancer(
-    Name='test-othos-lb',
-    Subnets=[
-        'subnet-054c4c9508bcf7e0e',
-        'subnet-0eef3203f25475c0a',
-        'subnet-0f706c8e478ddf6d3',
-        'subnet-0dd46293c76fd15c6',
-        'subnet-0826eed225ee08584',
-        'subnet-0c2a4c881b34306f9'
-    ],
-    
-    SecurityGroups=[
-        'sg-095f4e84a06cb3243'
-    ],
-    Scheme='internet-facing',
-    Tags=[
-        {
-            'Key': 'string',
-            'Value': 'string'
-        },
-    ],
-    Type='application',
-    IpAddressType='ipv4',
-    
-)
+def create_load_balancer():
+    try:
 
+        response = client.create_load_balancer(
+            Name='test-othos-lb',
+            Subnets=[
+                'subnet-054c4c9508bcf7e0e',
+                'subnet-0eef3203f25475c0a',
+                'subnet-0f706c8e478ddf6d3',
+                'subnet-0dd46293c76fd15c6',
+                'subnet-0826eed225ee08584',
+                'subnet-0c2a4c881b34306f9'
+            ],
+    
+            SecurityGroups=[
+                'sg-095f4e84a06cb3243'
+            ],
+            Scheme='internet-facing',
+            Tags=[
+                {
+                    'Key': 'string',
+                    'Value': 'string'
+                },
+            ],
+            Type='application',
+            IpAddressType='ipv4',
+        )
+        return response['targetgroup']
+    except Exception as e:
+        print(str(e))
+        return False
 response1 = client.describe_target_groups(
      Names=[
         'test-orthos-target-group',
